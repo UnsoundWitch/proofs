346333f1c9fdbbe9933d0a55ca24f2e9ef065620
sym-cereal <11177488+sym-cereal@users.noreply.github.com>
2021-03-03 21:09:31 +1100

    update stlc def

--- isabelle/stlc.thy
+++ isabelle/stlc.thy
@@ -1,222 +1,157 @@
-theory STLC
-imports Main
-begin
+theory stlc               
+  imports Main                          
+begin                
+                                  
+datatype ty = TyArw ty ty | TyBool | TyNat
+                                                       
+type_synonym id = nat
 
-datatype ty
-  = IntT
-  | BoolT
-  | ArrowT ty ty (infixr "\<rightarrow>" 200)
+type_synonym 'a alist = "(nat \<times> 'a) list"
 
-datatype const
-  = IntC int
-  | BoolC bool
+fun lookup :: "id \<Rightarrow> 'a alist \<Rightarrow> 'a option" where
+  "lookup i [] = None"
+| "lookup i ((x, t)#xs) =
+    (if i = x then
+       Some t
+     else
+       lookup i xs)"
 
-datatype opr
-  = Succ
-  | Prev
-  | IsZero
+datatype op = Plus | Mult | Eq
 
-types name = nat
+datatype tm =
+  TmBool bool | TmNat nat 
+  | TmIf tm tm tm | TmOp op tm tm
+  | TmVar id | TmApp tm tm | TmAbs id ty tm 
+  | TmLet id tm tm
 
-datatype stmt
-  = SLet name expr stmt
-  | SRet expr
-  | SCall name expr expr stmt
-  | STailCall expr expr 
+datatype val = BoolV bool | IntV nat | FunV id tm
+                                   
+inductive is_val where
+  v_abs : "is_val (TmAbs x T t)" 
+| v_nat : "is_val (TmNat n)" 
+| v_bool : "is_val (TmBool b)"          
 
-and expr 
-  = Var name
-  | Const const
-  | PrimApp opr expr
-  | Lam nat ty stmt
+print_theorems
 
-datatype val
-  = VConst const
-  | Closure nat ty stmt "(nat \<times> val) list"
+primrec op_type :: "op \<Rightarrow> ty" where
+  "op_type Plus = TyNat"
+| "op_type Mult = TyNat"
+| "op_type Eq = TyBool"
 
-section "Option Monad"
+type_synonym ctx = "ty alist"
 
-definition
-  option_bind :: "['a option, 'a => 'b option] => 'b option" where
-  "option_bind m f = (case m of None => None | Some r => f r)"
-declare option_bind_def[simp]
+inductive has_type :: "ctx \<Rightarrow> tm \<Rightarrow> ty \<Rightarrow> bool" where
+  T_Bool : "has_type _ (TmBool _) TyBool" 
+| T_Nat : "has_type _ (TmNat _) TyNat"
+| T_If : "has_type \<Gamma> e1 TyBool \<Longrightarrow>
+          has_type \<Gamma> e2 T \<Longrightarrow>
+          has_type \<Gamma> e3 T \<Longrightarrow>
+          has_type \<Gamma> (TmIf e1 e2 e3) T"
+| T_Op : "T = op_type op \<Longrightarrow>
+          has_type \<Gamma> e1 T \<Longrightarrow>
+          has_type \<Gamma> e2 T \<Longrightarrow>
+          has_type \<Gamma> (TmOp op e1 e2) T"
+| T_Var : "Some T = lookup i \<Gamma> \<Longrightarrow> has_type \<Gamma> (TmVar i) T"
+| T_App : "has_type \<Gamma> e2 T1 \<Longrightarrow> has_type \<Gamma> e1 (TyArw T1 T2) \<Longrightarrow> has_type \<Gamma> (TmApp e1 e2) T2"
+| T_Abs : "has_type ((i, T1)#\<Gamma>) e T2 \<Longrightarrow>
+           has_type \<Gamma> (TmAbs i T1 e) (TyArw T1 T2)"
+| T_Let : "has_type \<Gamma> e1 T1 \<Longrightarrow>
+           has_type ((i, T1)#\<Gamma>) e2 T2 \<Longrightarrow> has_type \<Gamma> (TLet i e1 e2) T2"
 
-syntax "_option_bind" :: "[pttrns,'a option,'b] => 'c" ("(_ := _;//_)" 0)
-translations "P := E; F" == "CONST option_bind E (%P. F)"
+primrec subst :: "id \<Rightarrow> tm \<Rightarrow> tm \<Rightarrow> tm" where
+  "subst _ _ (TmBool b) = (TmBool b)"
+| "subst _ _ (TmNat n) = (TmNat n)"
+| "subst x s (TmIf e1 e2 e3) = (TmIf (subst x s e1) (subst x s e2) (subst x s e3))"
+| "subst x s (TmOp op e1 e2) = (TmOp op (subst x s e1) (subst x s e2))"
+| "subst x s (TmVar i) = (if i = x then s else (TmVar i))"
+| "subst x s (TmApp e1 e2) = (TmApp (subst x s e1) (subst x s e2))"
+| "subst x s (TmAbs i ty e) = (if i = x then (TmAbs i ty e) else (TmAbs i ty (subst x s e)))"
+| "subst x s (TmLet i e1 e2) = 
+              (if i = x 
+               then (TmLet i (subst x s e1) e2) 
+               else (TmLet i (subst x s e1) (subst x s e2)))"
 
-definition return :: "'a \<Rightarrow> 'a option" where
-  "return x \<equiv> Some x"
-declare return_def[simp]
-definition error :: "'a option" where
-  "error \<equiv> None"
-declare error_def[simp]
+fun step_op :: "op \<Rightarrow> tm \<Rightarrow> tm \<Rightarrow> tm" where
+  "step_op Plus (TmNat i) (TmNat j) = TmNat (i + j)"
+| "step_op Mult (TmNat i) (TmNat j) = TmNat (i * j)"
+| "step_op Eq (TmNat i) (TmNat j) = TmBool (i = j)"
 
-section "Operational Semantics"
+inductive step :: "tm \<Rightarrow> tm \<Rightarrow> bool" where
+  ST_IfTrue: "step (TmIf (TmBool True) e1 e2) e1"
+| ST_IfFalse: "step (TmIf (TmBool False) e1 e2) e2"
+| ST_If1: "step e0 e0' \<Longrightarrow> step (TmIf e0 e1 e2) (TmIf e0' e1 e2)"
+| ST_Op1: "step e1 e1' \<Longrightarrow> step (TmOp op e1 e2) (TmOp op e1' e2)"
+| ST_Op2: "is_val v1 \<Longrightarrow> step e2 e2' \<Longrightarrow> step (TmOp op v1 e2) (TmOp op v1 e2')"
+| ST_OpRes: "is_val v1 \<Longrightarrow> is_val v2 \<Longrightarrow> step (TmOp op v1 v2) (step_op op v1 v2)"
+| ST_AppAbs: "is_val v \<Longrightarrow> step (TmApp (TmAbs i T e) v) (subst i v e)"
+| ST_App1: "step t1 t1' \<Longrightarrow> step (TmApp t1 t2) (TmApp t1' t2)"
+| ST_App2: "is_val v1 \<Longrightarrow> step t2 t2' \<Longrightarrow> step (TmApp v1 t2) (TmApp v1 t2')"
+| ST_Let1: "step e1 e1' \<Longrightarrow> step (TmLet i e1 e2) (TmLet i e1' e2)"
+| ST_Let2: "is_val v1 \<Longrightarrow> step (TmLet i v1 e2) (subst i v1 e2)"
+| ST_Cong: "step e1 e2 \<Longrightarrow> step e2 e3 \<Longrightarrow> step e1 e3"
 
-types env = "(nat \<times> val) list"
-types stack = "(stmt \<times> env) list"
-types state = "stmt \<times> env \<times> stack"
+print_theorems
 
-fun lookup :: "'a \<Rightarrow> ('a \<times> 'b) list \<Rightarrow> 'b option" where
-  "lookup x [] = None" |
-  "lookup x ((y,v)#bs) = (if x = y then Some v else lookup x bs)"
+lemma example_1:
+  shows "step (TmIf (TmOp Eq (TmNat 0) (TmNat 1)) (TmBool True) (TmBool False)) (TmBool False)"
+  apply (rule ST_Cong)
+   apply (rule ST_If1)
+   apply (rule ST_OpRes; simp add:is_val.simps)
+  apply simp
+  by (rule ST_IfFalse)
 
-fun delta :: "opr \<Rightarrow> val \<Rightarrow> val option" where
-  "delta Succ (VConst (IntC n)) = Some (VConst (IntC (n + 1)))" |
-  "delta Prev (VConst (IntC n)) = Some (VConst (IntC (n - 1)))" |
-  "delta IsZero (VConst (IntC n)) = Some (VConst (BoolC (n = 0)))" |
-  "delta f v = None"
+(*
+fun eval_op :: "op \<Rightarrow> tm \<Rightarrow> tm \<Rightarrow> tm option" where
+  "eval_op Plus (TmNat i) (TmNat j) = Some (TmNat (i + j))"
+| "eval_op Mult (TmNat i) (TmNat j) = Some (TmNat (i * j))"
+| "eval_op Eq (TmNat i) (TmNat j) = Some (TmBool (i = j))"
+| "eval_op _ _ _ = None"
 
-fun eval :: "expr \<Rightarrow> env \<Rightarrow> val option" where
-  "eval (Var x) \<rho> = lookup x \<rho>" |
-  "eval (Const c) \<rho> = return (VConst c)" |
-  "eval (PrimApp f e) \<rho> = (v := eval e \<rho>; delta f v)" |
-  "eval (Lam x T s) \<rho> = return (Closure x T s \<rho>)"
+type_synonym env = "tm alist"
 
-fun step :: "state \<Rightarrow> state option" where
-  "step (SLet x e s, \<rho>, k) =
-    (v := eval e \<rho>; 
-     return (s, (x,v)#\<rho> , k))" |
-  "step (SRet e, \<rho>, (SCall x e1 e2 s, \<rho>')#k) =
-      (v := eval e \<rho>;
-       return (s, (x,v)#\<rho>', k))" |
-  "step (SCall x e1 e2 s, \<rho>, k) =
-     (v1 := eval e1 \<rho>; v2 := eval e2 \<rho>;
-      case v1 of
-        Closure y T s' \<rho>' \<Rightarrow>
-          return (s', (y,v2)#\<rho>', (SCall x e1 e2 s,\<rho>)#k)
-      | _ \<Rightarrow> error)" |
-  "step (STailCall e1 e2, \<rho>, k) =
-     (v1 := eval e1 \<rho>; v2 := eval e2 \<rho>;
-      case v1 of
-        Closure y T s' \<rho>' \<Rightarrow>
-          return (s', (y,v2)#\<rho>', k)
-      | _ \<Rightarrow> error)" |
-  "step s = None"
+function eval :: "env \<Rightarrow> tm \<Rightarrow> tm option" where
+  "eval \<Delta> (TmBool b) = Some (TmBool b)"
+| "eval \<Delta> (TmNat n) = Some (TmNat n)"
+| "eval \<Delta> (TmIf e1 e2 e3) = 
+      (case eval \<Delta> e1 of
+        Some (TmBool True) \<Rightarrow> eval \<Delta> e2
+        | Some (TmBool False) \<Rightarrow> eval \<Delta> e3
+        | _ \<Rightarrow> None)"
+| "eval \<Delta> (TmOp op e1 e2) = (
+      let e1' = eval \<Delta> e1 in
+      let e2' = eval \<Delta> e2 in
+      case (e1', e2') of
+        (Some arg1, Some arg2) \<Rightarrow> eval_op op arg1 arg2
+        | _ \<Rightarrow> None)"
+| "eval \<Delta> (TmVar i) = lookup i \<Delta>"
+| "eval \<Delta> (TmApp e1 e2) = (
+      let e1' = eval \<Delta> e1 in
+      let e2' = eval \<Delta> e2 in
+      case (e1', e2') of
+       (Some (TmAbs i _ e3), Some arg) \<Rightarrow> eval ((i, e2)#\<Delta>) e3
+       | _ \<Rightarrow> None)"
+| "eval \<Delta> (TmAbs i ty e) = Some (TmAbs i ty e)"
+| "eval \<Delta> (TmLet i e1 e2) = (
+      case eval \<Delta> e1 of
+        Some x \<Rightarrow> eval ((i, x)#\<Delta>) e2
+        | _ \<Rightarrow> None)"
+                      apply auto
+  by (meson tm.exhaust)
 
-datatype result = Fun | Con const | Error | TimeOut
+termination eval
+  apply (rule local.termination)  
 
-primrec observe :: "val \<Rightarrow> result" where
-  "observe (VConst c) = Con c" |
-  "observe (Closure x T s \<rho>) = Fun"
+print_theorems
 
-definition final :: "state \<Rightarrow> bool" where
-  "final s \<equiv> (case s of (SRet e, \<rho>, []) \<Rightarrow> True | _ \<Rightarrow> False)"
-declare final_def[simp]
+lemma example1:
+  shows "eval [] (TmBool False) = Some (TmBool False)"
+  
+  by (simp add:eval.psimps)
 
-fun steps :: "nat \<Rightarrow> state \<Rightarrow> result" where
-  "steps 0 s = TimeOut" |
-  "steps (Suc n) (SRet e, \<rho>, []) =
-            (case eval e \<rho> of
-               None \<Rightarrow> Error
-            | Some v \<Rightarrow> observe v)" |
-  "steps (Suc n) s =
-         (case step s of
-           None \<Rightarrow> Error
-         | Some s' \<Rightarrow> steps n s')"
-
-definition run :: "stmt \<Rightarrow> result" where
-  "run s \<equiv> steps 1000000 (s,[],[])"
-
-types ty_env = "(name \<times> ty) list"
-
-primrec typeof :: "const \<Rightarrow> ty" where
-  "typeof (IntC n) = IntT" |
-  "typeof (BoolC b) = BoolT"
-
-primrec typeof_opr :: "opr \<Rightarrow> ty" where
-  "typeof_opr Succ = (IntT \<rightarrow> IntT)" |
-  "typeof_opr Prev = (IntT \<rightarrow> IntT)" |
-  "typeof_opr IsZero = (IntT \<rightarrow> BoolT)"
-
-inductive
-  wt_expr :: "ty_env \<Rightarrow> expr \<Rightarrow> ty \<Rightarrow> bool" ("_ \<turnstile>\<^isub>e _ : _" [60,60,60] 59) 
-  and wt_stmt :: "ty_env \<Rightarrow> stmt \<Rightarrow> ty \<Rightarrow> bool" ("_ \<turnstile>\<^isub>s _ : _" [60,60,60] 59) 
-  where
-  "lookup x \<Gamma> = Some A \<Longrightarrow> \<Gamma> \<turnstile>\<^isub>e Var x : A" |
-  "\<Gamma> \<turnstile>\<^isub>e Const c : typeof c" |
-  "\<lbrakk> typeof_opr f = A \<rightarrow> B; \<Gamma> \<turnstile>\<^isub>e e : A \<rbrakk> \<Longrightarrow> \<Gamma> \<turnstile>\<^isub>e PrimApp f e : B" |
-  "\<lbrakk> (x,A)#\<Gamma> \<turnstile>\<^isub>s s : B \<rbrakk> \<Longrightarrow>
-     \<Gamma> \<turnstile>\<^isub>e Lam x A s : (A \<rightarrow> B)" |
-
-  "\<lbrakk> \<Gamma> \<turnstile>\<^isub>e e : A; (x,A)#\<Gamma> \<turnstile>\<^isub>s s : B \<rbrakk> \<Longrightarrow>
-     \<Gamma> \<turnstile>\<^isub>s SLet x e s : B" |
-  "\<lbrakk> \<Gamma> \<turnstile>\<^isub>e e : A \<rbrakk> \<Longrightarrow>
-     \<Gamma> \<turnstile>\<^isub>s SRet e : A" |
-  "\<lbrakk> \<Gamma> \<turnstile>\<^isub>e e1 : A \<rightarrow> B; \<Gamma> \<turnstile>\<^isub>e e2 : A; (x,B)#\<Gamma> \<turnstile>\<^isub>s s : C \<rbrakk> \<Longrightarrow>
-     \<Gamma> \<turnstile>\<^isub>s SCall x e1 e2 s : C" |
-  "\<lbrakk> \<Gamma> \<turnstile>\<^isub>e e1 : A \<rightarrow> B; \<Gamma> \<turnstile>\<^isub>e e2 : A \<rbrakk> \<Longrightarrow>
-     \<Gamma> \<turnstile>\<^isub>s STailCall e1 e2 : B"
-
-thm wt_expr_wt_stmt.induct
-
-inductive wt_val :: "val \<Rightarrow> ty \<Rightarrow> bool" 
-  and wt_env :: "ty_env \<Rightarrow> env \<Rightarrow> bool" ("_ \<turnstile> _" [60,60] 59) where
-  wt_vc[intro!]: "typeof c = A \<Longrightarrow> wt_val (VConst c) A" |
-  wt_cl[intro!]: "\<lbrakk> \<Gamma> \<turnstile> E; (x,A)#\<Gamma> \<turnstile>\<^isub>s s : B \<rbrakk> \<Longrightarrow> 
-     wt_val (Closure x A s E) (A \<rightarrow> B)" |
-
-  wt_nil[intro!]: "[] \<turnstile> []" |
-  wt_cons[intro!]: "\<lbrakk> wt_val v A; \<Gamma> \<turnstile> E \<rbrakk> \<Longrightarrow> ((x,A)#\<Gamma>) \<turnstile> ((x,v)#E)"
-
-inductive wt_stack :: "stack \<Rightarrow> ty \<Rightarrow> ty \<Rightarrow> bool" ("_ : _ \<Rightarrow> _") where
-  nil_stack[intro!]: "[] : A \<Rightarrow> A" |
-  cons_stack[intro!]: "\<lbrakk> \<Gamma> \<turnstile> \<rho>; (x,A)#\<Gamma> \<turnstile>\<^isub>s s : B; k : B \<Rightarrow> C \<rbrakk> \<Longrightarrow>
-      (SCall x e1 e2 s,\<rho>)#k : A \<Rightarrow> C"
-
-inductive wt_state :: "state \<Rightarrow> ty \<Rightarrow> bool" where
-  wts[intro!]: "\<lbrakk> \<Gamma> \<turnstile> \<rho>; \<Gamma> \<turnstile>\<^isub>s s : A; k : A \<Rightarrow> B \<rbrakk> \<Longrightarrow>
-    wt_state (s, \<rho>, k) B"
-
-fun wt_result :: "result \<Rightarrow> ty \<Rightarrow> bool" where
-  "wt_result Fun (A \<rightarrow> B) = True" |
-  "wt_result (Con c) T = (typeof c = T)" |
-  "wt_result Error T = False"
-
-lemma delta_safe: 
-  assumes wtop: "typeof_opr f = A \<rightarrow> B"
-  and wtv: "wt_val v A"
-  shows "\<exists> v'. delta f v = Some v' \<and> wt_val v' B"
-  sorry
-
-lemma lookup_safe: 
-  assumes wtg: "\<Gamma> \<turnstile> \<rho>" and l: "lookup x \<Gamma> = Some A"
-  shows "\<exists> v. lookup x \<rho> = Some v \<and> wt_val v A"
-  sorry
-
-lemma eval_safe:
-  assumes wte: "\<Gamma> \<turnstile>\<^isub>e e : A"
-  and wtg: "\<Gamma> \<turnstile> \<rho>"
-  shows "\<exists> v. eval e \<rho> = Some v \<and> wt_val v A"
-  sorry
-
-lemma step_safe: 
-  assumes wtsA: "wt_state s A"
-  shows "final s \<or> (\<exists> s'. step s = Some s' \<and> wt_state s' A)"
-  sorry
-
-lemma steps_safe:
-  assumes wtsA: "wt_state s A"
-  shows "steps n s = TimeOut \<or> (\<exists> r. steps n s = r \<and> wt_result r A)"
-  using wtsA apply (induct n s rule: steps.induct)
-
-theorem type_safety:
-  assumes wts: "[] \<turnstile>\<^isub>s s : A"
-  shows "\<exists> r. run s = r \<and> (r = TimeOut \<or> wt_result r A)"
-proof -
-  from wts have 1: "wt_state (s,[],[]) A" by auto
-  let ?n = "1000000" and ?s = "(s,[],[])"
-  from 1 have 2:
-    "steps ?n ?s = TimeOut \<or> (\<exists> r. steps ?n ?s = r \<and> wt_result r A)"
-    by (rule steps_safe)
-  moreover { assume "steps ?n ?s = TimeOut"
-    with 2 have "\<exists> r. run s = r \<and> (r = TimeOut \<or> wt_result r A)"
-      by (auto simp: run_def)
-  } moreover { assume 3: "\<exists> r. steps ?n ?s = r \<and> wt_result r A"
-    from 3 obtain r where 4: "steps ?n ?s = r" and 5: "wt_result r A" by blast
-    from 4 have 6: "run s = r" by (simp add: run_def)
-    from 6 5 have "\<exists> r. run s = r \<and> (r = TimeOut \<or> wt_result r A)" by blast
-  } ultimately show ?thesis by blast
-qed
+lemma example1:
+  shows "eval [] (TmIf (TmOp Eq (TmNat 0) (TmNat 1)) (TmBool True) (TmBool False)) = Some (TmBool False)"
+  apply (simp add:eval_def)
+  by simp
+*)
 
 end
