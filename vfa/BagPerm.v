542ff5969c6457b272a480250e46fd3971b8074a
UnsoundWitch <11177488+UnsoundWitch@users.noreply.github.com>
2021-05-05 15:57:33 +1000

    add bagperm

--- vfa/BagPerm.v
+++ vfa/BagPerm.v
@@ -107,14 +107,34 @@ Definition is_a_sorting_algorithm' (f: list nat -> list nat) :=
 
 Lemma insert_bag: forall x l, bag_eqv (x::l) (insert x l).
 Proof.
-(* FILL IN HERE *) Admitted.
+  intros. induction l.
+  - constructor.
+  - simpl. destruct (a >=? x).
+    + apply bag_eqv_cons.
+      apply bag_eqv_cons.
+      apply bag_eqv_refl.
+    + apply bag_eqv_trans with (a :: x :: l).
+      unfold bag_eqv. intros. simpl.
+      rewrite Nat.add_assoc.
+      rewrite Nat.add_assoc.
+      omega.
+      apply bag_eqv_cons.
+      apply IHl.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard (sort_bag) 
 
     Now prove that sort preserves bag contents. *)
 Theorem sort_bag: forall l, bag_eqv l (sort l).
-(* FILL IN HERE *) Admitted.
+  intros.
+  induction l.
+  - constructor.
+  - simpl.
+    apply bag_eqv_trans with (a :: sort l).
+    apply bag_eqv_cons. apply IHl.
+    apply insert_bag.
+Qed.
 (** [] *)
 
 (** Now we wrap it all up.  *)
@@ -132,12 +152,12 @@ Qed.
 
       - [ ] easier with permutations,
       - [ ] easier with multisets
-      - [ ] about the same.
+      - [X] about the same.
 
    Regardless of "difficulty", which do you prefer / find easier to
    think about?
       - [ ] permutations or
-      - [ ] multisets
+      - [X] multisets
 
    Put an X in one box in each list. *)
 (* Do not modify the following line: *)
@@ -159,8 +179,21 @@ Definition manual_grade_for_permutations_vs_multiset : option (nat*string) := No
     [Permutation]: *)
 Lemma perm_bag:
   forall al bl : list nat,
-   Permutation al bl -> bag_eqv al bl. 
-(* FILL IN HERE *) Admitted.
+    Permutation al bl -> bag_eqv al bl.
+Proof.
+  intros.
+  induction H; try constructor.
+  - apply bag_eqv_cons.
+    apply IHPermutation.
+  - unfold bag_eqv. intros.
+    simpl.
+    rewrite Nat.add_assoc.
+    rewrite Nat.add_assoc.
+    omega.
+  - eapply bag_eqv_trans.
+    apply IHPermutation1.
+    apply IHPermutation2.
+Qed.
 (** [] *)
 
 (** The other direction,
@@ -172,7 +205,15 @@ Lemma perm_bag:
 (** **** Exercise: 2 stars, advanced (bag_nil_inv)  *)
 Lemma bag_nil_inv : forall b, bag_eqv [] b -> b = []. 
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros.
+  induction b.
+  reflexivity.
+  unfold bag_eqv in *.
+  specialize H with a.
+  simpl in H.
+  rewrite Nat.eqb_refl in H.
+  inversion H.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced (bag_cons_inv)  *)
@@ -182,21 +223,93 @@ Lemma bag_cons_inv : forall l x n,
       l = l1 ++ x :: l2
       /\ count x (l1 ++ l2) = n.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction l; intros.
+  - simpl in H. inversion H.
+  - destruct n.
+    + destruct (a =? x) eqn:E0.
+      * simpl in H. rewrite E0 in H.
+        exists [], l.
+        split; simpl.
+        rewrite Nat.eqb_eq in E0.
+        rewrite E0. reflexivity.
+        omega.
+      * simpl in H. rewrite E0 in H.
+        specialize IHl with x 0.
+        apply IHl in H.
+        destruct H. destruct H. destruct H.
+        exists (a::x0), x1. split.
+        rewrite H. reflexivity.
+        simpl. rewrite E0. omega.
+    + destruct (a =? x) eqn:E0.
+      * simpl in H. rewrite E0 in H.
+        exists [], l.
+        split; simpl.
+        rewrite Nat.eqb_eq in E0.
+        subst. reflexivity.
+        inversion H. reflexivity.
+      * simpl in H. rewrite E0 in H.
+        specialize IHl with x (S n).
+        apply IHl in H.
+        destruct H. destruct H. destruct H.
+        exists (a::x0), x1. split.
+        rewrite H. reflexivity.
+        simpl. rewrite E0. omega.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, advanced (count_insert_other)  *)
 Lemma count_insert_other : forall l1 l2 x y,
     y <> x -> count y (l1 ++ x :: l2) = count y (l1 ++ l2).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction l1;
+    intros.
+  - simpl.
+    apply Nat.neq_sym in H.
+    rewrite <- Nat.eqb_neq in H.
+    rewrite H. reflexivity.
+  - simpl in *.
+    destruct (a =? y);
+      try rewrite Nat.succ_inj_wd;
+      specialize IHl1 with l2 x y;
+      apply IHl1 in H;
+      apply H.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced (bag_perm)  *)
 Lemma bag_perm:
   forall al bl, bag_eqv al bl -> Permutation al bl.
 Proof.
-(* FILL IN HERE *) Admitted.
+  induction al.
+  - intros.
+    apply bag_nil_inv in H.
+    rewrite H. constructor.
+  - generalize dependent al.
+    induction bl; intros.
+    + unfold bag_eqv in H.
+      specialize H with a.
+      simpl in H. rewrite Nat.eqb_refl in H.
+      inversion H.
+    + destruct (a =? a0) eqn:E0.
+      * rewrite Nat.eqb_eq in E0.
+        subst. constructor.
+        apply IHal.
+        unfold bag_eqv in *.
+        intros.
+        specialize H with n.
+        destruct (a0 =? n) eqn:Eb.
+        rewrite -> Nat.eqb_eq in Eb. subst.
+        simpl in H. omega.
+        simpl in H. rewrite Eb in H. omega.
+      * rewrite Nat.eqb_neq in E0.
+        unfold bag_eqv in *.
+        admit.
+(* simple guess:
+   the following might be destruct n to be a and b 
+   and the enumerate the cases eventually we get a contradiction 
+   on most cases
+ *)
+ Admitted.
 (** [] *)
 
 (* ################################################################# *)
